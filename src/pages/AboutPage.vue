d830773c2397eda0401fc13afc8f5f92705a1679
Edilson Mutisse <98753172+Mutisse@users.noreply.github.com>
2025-10-17 12:11:40 +0200

    fix: corrige AboutPage.vue

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page class="flex flex-center">
     <div class="text-center">
-      <h4 class="text-h4 text-weight-bold q-mb-md">Pesquisas</h4>
-      <p class="text-grey-7">Página de pesquisas em desenvolvimento</p>
+      <h4 class="text-h4 text-weight-bold q-mb-md">Sobre</h4>
+      <p class="text-grey-7">Página sobre o projeto Moz-Pesquisa</p>
       <q-btn 
         label="Voltar para Home" 
         color="primary" 
